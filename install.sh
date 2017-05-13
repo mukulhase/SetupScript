220505223e7b3b16e22a58402585145acb593eb4
Mukul Hase <mukulhase@gmail.com>
2017-05-13 18:30:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable
 sudo add-apt-repository ppa:webupd8team/sublime-text-3;
 sudo apt-get update;
 sudo apt install nautilus-dropbox python-gpgme
-sudo apt-get install vim shutter openssh-server git zsh google-chrome-stable sublime-text-installer build-essential libssl-dev terminator;
+sudo apt-get install curl vim shutter openssh-server git zsh google-chrome-stable sublime-text-installer build-essential libssl-dev terminator;
 curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
 wget -qO- 'https://raw.githubusercontent.com/mukulhase/AutoVPN/master/autovpn.sh' | sudo bash /dev/stdin mukul.hase@students.iiit.ac.in xxx;
 wget https://bootstrap.pypa.io/get-pip.py
